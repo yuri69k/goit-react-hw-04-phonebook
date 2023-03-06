ee5527be6fa5b863672b0a1e8bdf955207587cf7
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-03-06 16:13:08 +0200

    fix filter

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 
-const useLocalStorage = key => {
-  const defaultValue = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+const useLocalStorage = (key, defaultValue) => {
+  // const defaultValue = [
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ];
   const fromStorage = JSON.parse(window.localStorage.getItem(key));
 
   const [state, setState] = useState(() =>
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,13 @@ import shortid from 'shortid';
 import Notiflix from 'notiflix';
 
 const App = () => {
-  const [contacts, setContacts] = useLocalStorage('contacts', []);
+   const defaultValue = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const [contacts, setContacts] = useLocalStorage('contacts', defaultValue);
   const [filter, setFilter] = useState('');
 
 
@@ -28,13 +34,17 @@ const App = () => {
 
 
   const onFilterInput = value => {
-    setFilter(value);
+    const normalizedStr = value.trim().toLowerCase();
+    setFilter(normalizedStr);
   };
 
-  const filteredContacts =  useMemo(() => {
-    return [...contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())),];
-  },[contacts,filter]);
+  const filteredContacts =  () => {
+      return [
+      ...contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter)
+      ),
+    ];
+  };
 
   const deleteContact = id => {
     setContacts(contacts.filter(contact => contact.id !== id));
